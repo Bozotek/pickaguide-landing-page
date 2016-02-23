288d00baa1cd46c99faf992bed4f94a88eb10c14
Bozotek <sangoi.lucas@gmail.com>
2016-02-23 12:27:14 +0100

    first form guide

--- contact/index.php
+++ contact/index.php
@@ -4,14 +4,23 @@
 	<head>
 		<title>About</title>
 		<meta charset="utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="contact.css" />
 		<link rel="icon" href="/GeekAdminTools-v0.1/src/images/ghiss.ico" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 	</head>
-	
-	<body>
-        
+
+	<body id="mainBackground">
+		<?php include '../menu.html'; ?>
+		<div class="contentBox">
+			<p>
+				Welcome on the guide form ! </br>Please fill some informations and you will be part of our community !
+			</p>
+		</div>
+		<form id="guide_form" style="text-align:center;">
+			<input type="text" name="email" placeholder="Enter your email" class="input">
+			<input type="submit" name="submit" class="submit" value="Apply" onclick="return submitGuideInfos();" >
+		</form>
 	</body>
-	
+
 </html>
