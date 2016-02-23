bb38c4916ac2d69cc9d2086b99c190fa86315487
Bozotek <sangoi.lucas@gmail.com>
2016-02-23 16:08:32 +0100

    name form guide

--- contact/index.php
+++ contact/index.php
@@ -23,10 +23,10 @@
 		<div id="guide_form_wrapper" style="text-align:center;">
 			<div id="messageBox"><p id="message"></p></div>
 			<form id="guide_form">
-				<input type="text" name="title" placeholder="Enter a title announcement" class="input">
-				<input type="text" name="price" placeholder="Enter a price" class="input" id="price">
-				<input type="number" name="hours" placeholder="Enter nb hours" class="input" id="hours" min="1" max="4"></code>
-				<textarea type="text" name="description" placeholder="Enter a description" class="input" rows="4" cols="50"></textarea>
+				<input type="text" name="title" placeholder="Title" class="input">
+				<input type="text" name="price" placeholder="Price" class="input" id="price">
+				<input type="number" name="hours" placeholder="Hours" class="input" id="hours" min="1" max="4"></code>
+				<textarea type="text" name="description" placeholder="Description" class="input" rows="4" cols="50"></textarea>
 				<input type="submit" name="submit" class="submit" value="Apply" onclick="return submitGuideInfos();" >
 				<!-- <input type="file" name="nom" /> -->
 			</form>
