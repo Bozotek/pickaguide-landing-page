b32adff52cd96b842356762dc131f625e6f49322
Bozotek <sangoi.lucas@gmail.com>
2016-02-23 16:58:15 +0100

    change routes css

--- contact/index.php
+++ contact/index.php
@@ -6,7 +6,7 @@
 		<meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="contact.css" />
-		<link rel="icon" href="/GeekAdminTools-v0.1/src/images/ghiss.ico" />
+		<link rel="icon" href="/src/images/ghiss.ico" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script src="/js/form.js"></script>
 	</head>
