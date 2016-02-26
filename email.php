f43fa92eeaaf9180000c02ac7a303817b30ed997
Alex <alexander.saenen@epitech.eu>
2016-02-26 05:31:16 +0100

    voila

--- email.php
+++ email.php
@@ -28,9 +28,9 @@
     $body = "Bonjour,\nNous avons le plaisir de vous informer qu'un visiteur s'intéresse à vous et souhaite passer du temps à découvrir la ville avec vous !";
     $body += "\nContactez le rapidement par mail sur " . $from["email"] . " ou sur son portable " . $from["tel"] . " !";
     $body += "En espérant que vous passerez du bon temps ensemble !\nPickaGuide";
-
-    SparkPost::setConfig(["key"=>'4feda1a12fb0d4ce0b2c4fd47ce5bef210bc0fcc']);
 /*
+    SparkPost::setConfig(["key"=>'4feda1a12fb0d4ce0b2c4fd47ce5bef210bc0fcc']);
+
     try {
         // Build your email and send it!
         Transmission::send(array('campaign'=>'first-mailing',
