21be55434bf0a3f230971bb8d4b353f5be7b3a5e
Alex <alexander.saenen@epitech.eu>
2016-02-26 06:26:16 +0100

    fuck it

--- email.php
+++ email.php
@@ -25,9 +25,9 @@
     if ($from == null)
       throw new Exception("Une erreur est survenue");
 
-    $html = "Bonjour!<br/><br/>Nous avons le plaisir de vous informer qu'un visiteur s'intéresse à vous et souhaite passer du temps à découvrir la ville avec vous !"
-      + "<br/>Contactez le rapidement par mail sur " . $from["email"] . " ou sur son portable " . $from["tel"] . " !"
-      + "<br/><br/>En espérant que vous passerez du bon temps ensemble !<br/><br/>PickaGuide";
+    $html = "Bonjour!<br/><br/>Nous avons le plaisir de vous informer qu'un visiteur s'intéresse à vous et souhaite passer du temps à découvrir la ville avec vous !
+      <br/>Contactez le rapidement par mail sur " . $from["email"] . " ou sur son portable " . $from["tel"] . " !
+      <br/><br/>En espérant que vous passerez du bon temps ensemble !<br/><br/>PickaGuide";
 
     $from = array('no-reply@pickaguide.fr' => 'Service Pickaguide');
     $to = array('alexander.saenen@epitech.eu' => 'Raloufah');
