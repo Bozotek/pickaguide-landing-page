1115e5e439bad72e00796cda2e911fecfaf48a88
Alex <alexander.saenen@epitech.eu>
2016-02-26 06:14:16 +0100

    fuck it

--- email.php
+++ email.php
@@ -29,44 +29,28 @@
     $body += "\nContactez le rapidement par mail sur " . $from["email"] . " ou sur son portable " . $from["tel"] . " !";
     $body += "En espérant que vous passerez du bon temps ensemble !\nPickaGuide";
 
-    // This is your From email address
     $from = array('no-reply@pickaguide.fr' => 'Service de Pickaguide');
-    // Email recipients
-    $to = array(
-         'alexander.saenen@epitech.eu'=>'Raloufah'
-    );
-    // Email subject
-    $subject = 'Example PHP Email';
+    $to = array('alexander.saenen@epitech.eu' => 'Raloufah');
+    $subject = 'Un visiteur vous sollicite !';
 
-    // Login credentials
     $username = 'app47628710@heroku.com';
     $password = 'vgfryzxx3287';
 
-    // Setup Swift mailer parameters
     $transport = Swift_SmtpTransport::newInstance('smtp.sendgrid.net', 587);
     $transport->setUsername($username);
     $transport->setPassword($password);
     $swift = Swift_Mailer::newInstance($transport);
 
-    // Create a message (subject)
     $message = new Swift_Message($subject);
 
-    // attach the body of the email
     $message->setFrom($from);
-    $message->setBody($body, 'text/plain');
+    $message->setBody($body, 'text/html');
     $message->setTo($to);
 
-    // send message
-    if ($recipients = $swift->send($message, $failures))
+    if (!$recipients = $swift->send($message, $failures))
     {
-       // This will let us know how many users received this message
-       echo 'Message sent out to '.$recipients.' users';
-    }
-    // something went wrong =(
-    else
-    {
-       echo "Something went wrong - ";
-       print_r($failures);
+      $response["status"] = false;
+      $response["message"] = $failures;
     }
   } catch (Exception $e) {
     $response["status"] = false;
